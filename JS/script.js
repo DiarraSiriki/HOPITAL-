2ad8efd1b745fd847bfb5efb951ec2920bce2a9b
Diarrassouba Siriki <sdiarrassouba075@gmail.com>
2026-04-16 14:54:33 +0000

    creation de la section specialites

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+// 
+// 
+// let addition = (nombrea,nombreb) =>  nombrea + nombreb    ;
+
+// console.log(addition(5,10));
+// let addition = prenom => "Au revoir " + prenom
+// console.log(addition("Nicolas")
+
+
+// let articles = {
+//      article1:{
+//         title: "mon prmier article",
+//         content: "Lorem ."   
+//      },
+//      article2:{
+//         title: "mon second article",
+//         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque."                       
+//      }
+// }
+// console.log(articles.article1.title);   
+
+// class Utilisateur {
+//     constructor(nom, prenom, age) {
+//         this.nom = nom;
+//         this.prenom = prenom;
+//         this.age = age;
+//     }
+// }
+// let 
